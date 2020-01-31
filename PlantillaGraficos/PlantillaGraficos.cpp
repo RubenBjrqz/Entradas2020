c44ec2ef0cfcd167770746e01d3f3df00faf1906
Rubén Bojórquez <42181319+RubenBjrqz@users.noreply.github.com>
2020-01-31 08:58:20 -0700

    Movimiento fluido

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -13,7 +13,11 @@
 
 using namespace std;
 
+//Declarar una ventana
+GLFWwindow* window;
 float posXTriangulo = 0.0f, posYTriangulo = 0.0f;
+double tiempoActual, tiempoAnterior;
+double velocidadTriangulo = 0.7;
 
 void teclado_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -37,7 +41,27 @@ void teclado_callback(GLFWwindow* window, int key, int scancode, int action, int
 
 void actualizar() 
 {
-	//posXTriangulo += 0.00001;
+	tiempoActual = glfwGetTime();
+	double tiempoDiferencial = tiempoActual - tiempoAnterior;
+	
+	int  estadoDerecha = glfwGetKey(window, GLFW_KEY_RIGHT);
+	if (estadoDerecha == GLFW_PRESS) {
+		posXTriangulo += velocidadTriangulo * tiempoDiferencial;
+	}
+	int estadoArriba = glfwGetKey(window, GLFW_KEY_UP);
+	if (estadoArriba == GLFW_PRESS) {
+		posYTriangulo += velocidadTriangulo * tiempoDiferencial;
+	}
+	int estadoIzquierda = glfwGetKey(window, GLFW_KEY_LEFT);
+	if (estadoIzquierda == GLFW_PRESS) {
+		posXTriangulo -= velocidadTriangulo * tiempoDiferencial;
+	}
+	int estadoAbajo = glfwGetKey(window, GLFW_KEY_DOWN);
+	if (estadoAbajo == GLFW_PRESS) {
+		posYTriangulo -= velocidadTriangulo * tiempoDiferencial;
+	}
+
+	tiempoAnterior = tiempoActual;
 }
 
 void dibujar()
@@ -62,8 +86,6 @@ void dibujar()
 
 int main()
 {
-	//Declarar una ventana
-	GLFWwindow* window;
 
 	//Si no se pudo iniciar GLFW terminamos ejecucion
 	if (!glfwInit())
@@ -97,8 +119,11 @@ int main()
 
 	//Establecemos que con cada evento de teclado
 	//se llama a la función teclado_callback
-	glfwSetKeyCallback(window, teclado_callback);
+	//glfwSetKeyCallback(window, teclado_callback);
 
+	tiempoActual = glfwGetTime();
+	tiempoAnterior = tiempoActual;
+	
 	//Ciclo de dibujo (Draw loop)
 	while (!glfwWindowShouldClose(window))
 	{
